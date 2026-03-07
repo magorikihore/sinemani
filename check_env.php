633b30467dd135020a6daac4a26b1646ef33202c
magorikihore <magorikihore@users.noreply.github.com>
2026-03-07 09:02:56 -0800

    Remove temp debug files

--- check_env.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Check what the env parser reads
-require '/var/www/dramabox/vendor/autoload.php';
-$dotenv = \Dotenv\Dotenv::createImmutable('/var/www/dramabox');
-$dotenv->load();
-
-echo "DB_PASSWORD from env: [" . $_ENV['DB_PASSWORD'] . "]\n";
-echo "DB_USERNAME from env: [" . $_ENV['DB_USERNAME'] . "]\n";
